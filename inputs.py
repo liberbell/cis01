9923682b4cde8fd8d0c3ef1bb6467d5da7ea607c
Liberbell <liberbell@gmail.com>
2020-12-23 08:50:08 +0900

    inputs7

--- inputs.py
+++ inputs.py
@@ -9,5 +9,5 @@ if n < 5:
     print("n is less than five")
 elif n == 5:
     print("n is equal to five")
-    else:
-        print("n is bigger than five")
+else:
+    print("n is bigger than five")
