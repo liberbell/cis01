b7657b6e03148e4a57cceeb856836a2ba499a426
Liberbell <liberbell@gmail.com>
2020-12-25 08:45:58 +0900

    dic1

--- dic.py
+++ dic.py
@@ -1,6 +1,14 @@
+from collections import OrderedDict
+
 d = {"apple": 5, "peers": 2, "orange": 8}
 print(d)
 print(d["peers"])
 d["peer"] = 12
 d["nabana"] = 4
 print(d)
+
+od = OrderedDict()
+od["apple"] = 5
+od["peer"] = 2
+od["orange"] = 8
+print(od["peer"])
