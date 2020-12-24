44828e11ea778cf246b2c2533182e16c68ca12df
Liberbell <liberbell@gmail.com>
2020-12-25 08:54:54 +0900

    forloops6

--- forloops.py
+++ forloops.py
@@ -5,3 +5,7 @@ for i in range(5):
 names = ["chris", "iftach", "jay"]
 for name in names:
     print(name)
+
+fruit_inventory = {"apples": 5, "peers": 2, "orange": 8}
+for fruit in fruit_inventory:
+    print(fruit)
