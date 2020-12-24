2b53a16b17891c37c4ff29f4e3d9957091dece7d
Liberbell <liberbell@gmail.com>
2020-12-25 08:57:00 +0900

    forloops7

--- forloops.py
+++ forloops.py
@@ -10,3 +10,6 @@ fruit_inventory = {"apples": 5, "peers": 2, "orange": 8}
 print(list(fruit_inventory.items()))
 for fruit in fruit_inventory:
     print(fruit)
+
+for fruit in fruit_inventory.items():
+    print(fruit)
