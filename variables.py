d9a15ce68bdca11663482c7aa2ba5c5259840ca0
Liberbell <liberbell@gmail.com>
2020-12-26 17:18:14 +0900

    variables3

--- variables.py
+++ variables.py
@@ -4,3 +4,5 @@ def my_func(argument_variable):
     local_variable = "I am a local variable."
 
     print(module_variable, "... and I can be accessed inside a function.")
+    print(argument_variable, "... and I can be passed to a function.")
+    print(local_variable, "... and I can ONLY accessed inside a function.")
