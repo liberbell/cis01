90bff15599e98a951681d9059b52bd37a41e1fe5
Liberbell <liberbell@gmail.com>
2020-12-26 17:43:53 +0900

    fortune6

--- fortune_cookie.py
+++ fortune_cookie.py
@@ -37,7 +37,7 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     fortune = generate_fortune()
     num = generate_lucky_numbers(how_many_lucky_numbers)
 
-    return "{fortune}\nLucky Numbers: {lucky_numbers}".format(text1 = fortune,lucky_numbers = num)
+    return "{fortune}\nLucky Numbers: {lucky_numbers}".format(fortune = fortune,lucky_numbers = num)
 
     raise NotImplementedError()
 
